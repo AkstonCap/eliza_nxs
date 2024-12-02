51e693ae97d8e9715c5342335549bb8b9ab726bc
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-02 21:18:27 +0100

    Created starting point for Nexus plugin.

---
+++ packages/plugin-nexus/src/index.ts
@@ -0,0 +1,12 @@
+import { Plugin } from "@ai16z/eliza";
+
+export const nexusPlugin: Plugin = {
+    name: "nexus",
+    description: "Nexus Plugin for Eliza",
+    actions: [
+    ],
+    evaluators: [],
+    providers: [],
+};
+
+export default nexusPlugin;
