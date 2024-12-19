5ad2ca3872a9ecbb248c1de148194533a3593441
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-12-19 19:46:55 +0100

    Update index.ts

--- packages/plugin-nexus/src/index.ts
+++ packages/plugin-nexus/src/index.ts
@@ -1,9 +1,13 @@
 import { Plugin } from "@ai16z/eliza";
+//import { createSession } from "./actions/createSession";
+//import { createOrder } from "./actions/createOrder";
 
 export const nexusPlugin: Plugin = {
     name: "nexus",
     description: "Nexus Plugin for Eliza",
     actions: [
+        createSession,
+        createOrder,
     ],
     evaluators: [],
     providers: [],
